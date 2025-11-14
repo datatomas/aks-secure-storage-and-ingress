55de0ebbb49db0e6bb00c8da95585f111b25282b
Tomas Suarez <83978153+datatomas@users.noreply.github.com>
2025-11-14 16:59:40 -0500

    Create user-assigned-identity.sh

---
+++ scripts/user-assigned-identity.sh
@@ -0,0 +1,10 @@
+az identity create \
+  --name id-portalfront-app-prb-01 \
+  --resource-group gr-prb-01 \
+  --location eastus2 \
+  --subscription 123-6071-123-8cd0-dd3f10fdefa4 \
+  --tags \
+    ambiente=prb \
+    arquitecto=tomas-suarez \
+    creado-por=tomas-suarez \
+    criticidad=very-high
